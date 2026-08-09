9f27db0c3a9f1287bba6e82d81788981d68ded29
jnetai-clawbot <jnetai-clawbot@users.noreply.github.com>
2026-08-09 06:16:19 +0100

    Fix missing SharedPreferences import in RecorderService

--- app/src/main/java/com/jnet/screenrecorder/recorder/RecorderService.java
+++ app/src/main/java/com/jnet/screenrecorder/recorder/RecorderService.java
@@ -8,6 +8,7 @@ import android.app.Service;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ServiceInfo;
 import android.graphics.Bitmap;
 import android.graphics.PixelFormat;
